b6a15791d07a04ad864e627dd93b086f7d3a5dca
Bhaskar Raj <124422747+BhaskarRaj22@users.noreply.github.com>
2024-12-20 08:53:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const tabItems = document.getElementById('.tab-item');
+const tabContentItems = document.getElementById('.tab-content-item');
+
+// Select tab content item
+function selectItems(e) {
+    // Remove all show and border classes
+    removeBorder();
+    removeShow();
+    // Add border to current tab item
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItems = document.getElementById(`#${this.id}-content`);
+    // Add show class
+    tabContentItems.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove('tab-border');
+    });
+}
+
+// Remove show class from all content items
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show');
+    });
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItems);
+});
